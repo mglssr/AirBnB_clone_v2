d604a61540e96d893d15501a221f327ba983e1da
ezesilva95 <4381@holbertonschool.com>
2022-08-10 11:49:58 -0700

    update task 10

--- models/amenity.py
+++ models/amenity.py
@@ -3,6 +3,8 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Bas):
