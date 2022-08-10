9b874eda3154396938065096031b0715450da595
mglssr <iglesiasmaia6@gmail.com>
2022-08-10 08:20:05 -0700

    nullables

--- models/user.py
+++ models/user.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -12,8 +11,8 @@ class User(BaseModel, Base):
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
     places = relationship("Place", backref="user",
                           cascade="all, delete, delete-orphan")
     reviews = relationship("Review", backref="user",
