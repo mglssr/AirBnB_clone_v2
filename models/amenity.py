ba8d787c6199cf8cfb83bb3f141603a731d5ba57
ezesilva95 <4381@holbertonschool.com>
2022-08-10 14:59:33 -0700

    update

--- models/amenity.py
+++ models/amenity.py
@@ -3,16 +3,14 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
 from models.place import place_amenity
 
 
-class Amenity(BaseModel, Bas):
+class Amenity(BaseModel, Base):
     """
     Task 10
     """
     __tablename__ = "amenities"
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-        place_amenities = relationship("Place", secondary=place_amenity,
-                                       backref="amenities")
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity,
+                                       viewonly=True)
