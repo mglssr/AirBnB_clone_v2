c3576db63f051c3c7ca64ac0128eed56c101b20d
ezesilva95 <4381@holbertonschool.com>
2022-08-10 15:27:54 -0700

    pep8 fix

--- models/amenity.py
+++ models/amenity.py
@@ -13,4 +13,4 @@ class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary=place_amenity,
-                                       viewonly=True)
+                                   viewonly=True)
