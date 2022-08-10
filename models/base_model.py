76f60e77845ba5e35fe529716c9ff76112cbeee3
ezesilva95 <4381@holbertonschool.com>
2022-08-10 14:59:56 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -21,6 +21,7 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+        """
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -32,6 +33,17 @@ class BaseModel:
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
             self.__dict__.update(kwargs)
+            """
+        if kwargs is not None and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == "created_at" or key == "updated_at":
+                        value = datetime.fromisoformat(value)
+                    setattr(self, key, value)
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
