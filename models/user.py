6312f999c1d1780242bff150968fe4ff4efd5335
mglssr <iglesiasmaia6@gmail.com>
2022-08-10 08:13:39 -0700

    Revert "going back to last changes"
    This reverts commit f59407968ac3dc5745caee07cda38c831db27f6e.

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -8,10 +9,11 @@ from sqlalchemy.orm import relationship
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
     places = relationship("Place", backref="user",
                           cascade="all, delete, delete-orphan")
     reviews = relationship("Review", backref="user",
