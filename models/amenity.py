fe97f55317ec4de4e39658914a06cd662a4977e8
mglssr <iglesiasmaia6@gmail.com>
2022-08-10 08:06:55 -0700

    going back to last changes

--- models/amenity.py
+++ models/amenity.py
@@ -3,11 +3,6 @@
 
 
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
-from sqlalchemy.orm import relationship
-from models.place import place_amenity
 
 class Amenity(BaseModel, Base):
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity")
+    name = ""
--- models/place.py
+++ models/place.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
-from sqlalchemy.orm import relationship
 import models
-from os import getenv
 
-metadata = Base.metadata
-
-place_amenity = Table('place_amenities', metadata, Column("place_id", String(60), ForeignKey("places.id"), primary_key=True, nullable=False), Column("amenity_id", String(60), ForeignKey("amenities.id"), primary_key=True, nullable=False))
 
 class Place(BaseModel, Base):
     """A place to stay"""
@@ -27,8 +21,6 @@ class Place(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         reviews = relationship("Review", backref="place",
                            cascade="all, delete, delete-orphan")
-        amenities = relationship("Amenity", secondary="place_amenity", viewonly=False, overlaps = "place_amenities")
-
     else:
         @property
         def reviews(self):
@@ -38,19 +30,3 @@ class Place(BaseModel, Base):
                 if review.state_id == self.id:
                     list_review.append(review)
             return list_review
-        @property
-        def amenities(self):
-            """amenities getter"""
-            list_amenities = []
-            for amenity in models.storage.all("Amenity").value():
-                if amenity.id == self.id:
-                    list_amenities.append(ameinity)
-                return list_amenities
-        
-        @amenities.setter
-        def amenities(self):
-            """amenities setter"""
-            amenity_ids = []
-            for amenity in models.storage.all("Amenity").value():
-                amenity_ids.append(amenity.id)
-
