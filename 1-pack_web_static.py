44df56c61f300df9b6f7cb0d8b4bbbcfa05ad7ea
mglssr <iglesiasmaia6@gmail.com>
2022-08-25 17:50:27 +0000

    whitespace

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Fabric script that generates a .tgz archive from the 
+"""Fabric script that generates a .tgz archive from the
 contents of the web_static."""
 
 from fabric.api import local
