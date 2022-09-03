c3780e297c571b563272c15b4ebb5b58a766a4e3
mglssr <iglesiasmaia6@gmail.com>
2022-09-03 23:51:50 +0000

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,13 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """when request route "/" displays “Hello HBNB!”"""
+    return "Hello HBNB!"
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
-    @app.route("/", strict_slashes=False)
-    def hello_hbnb():
-        """when request route "/" displays “Hello HBNB!”"""
-        return (f"Hello HBNB!")
