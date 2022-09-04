af612c8438ce70d85d23d49621bd86ef26193078
mglssr <iglesiasmaia6@gmail.com>
2022-09-04 00:11:55 +0000

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """when request route "/" displays “Hello HBNB!”"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """generic comment"""
+    return "HBNB"
+
+
+@app.rpute("/c/<text_>", strict_slashes=False)
+def c_(txt):
+    """generic comment"""
+    txt = txt.replace("_", " ")
+    return f"C {txt}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
