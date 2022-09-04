8d997b232d95314b187596201ee6d1cfac1e49ed
mglssr <iglesiasmaia6@gmail.com>
2022-09-04 00:34:04 +0000

    task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """when request route "/" displays “Hello HBNB!”"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """generic comment"""
+    return "HBNB"
+
+
+@app.route("/c/<txt>", strict_slashes=False)
+def c_(txt):
+    """generic comment"""
+    txt = txt.replace("_", " ")
+    return "C {}".format(txt)
+
+
+@app.route("/Python/(<txt>)", strict_slashes=False)
+def c_(txt):
+    """generic comment"""
+    if not txt:
+        txt = "is cool"
+    else:
+        txt = txt.replace("_", " ")
+    return "Python {}".format(txt)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
