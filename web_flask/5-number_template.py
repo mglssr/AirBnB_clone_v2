eb8fe95392bd6da4f3328db4d700d2cd12292b79
mglssr <iglesiasmaia6@gmail.com>
2022-09-04 20:17:14 +0000

    task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """when request route "/" displays “Hello HBNB!”"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """generic comment"""
+    return "HBNB"
+
+
+@app.route("/c/<txt>", strict_slashes=False)
+def c_(txt):
+    """generic comment"""
+    txt = txt.replace("_", " ")
+    return "C {}".format(txt)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<txt>", strict_slashes=False)
+def python_(txt="is cool"):
+    """generic comment"""
+    txt = txt.replace("_", " ")
+    return "Python {}".format(txt)
+
+
+@app.route("/number/<int:my_num>", strict_slashes=False)
+def number(my_num):
+    """generic comment"""
+    return "{} is a number".format(my_num)
+
+
+@app.route("/number_template/<int: my_num>", strict_slashes=False)
+def my_number(my_num):
+    from flask import render_template
+    """generic comment"""
+    return render_template('5-number.html', num=my_num)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
