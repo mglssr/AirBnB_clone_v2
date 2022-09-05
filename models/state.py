a05207b2db67441a74633959268fa4a50b050b37
mglssr <iglesiasmaia6@gmail.com>
2022-09-05 15:59:30 +0000

    pycode

--- models/state.py
+++ models/state.py
@@ -12,11 +12,11 @@ from os import getenv
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    
+
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state",
-                          cascade="all, delete, delete-orphan")
+                              cascade="all, delete, delete-orphan")
     else:
         @property
         def cities(self):
