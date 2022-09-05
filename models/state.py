899535d57e471db6a4c2ebabb4eeb22df04f2cfd
mglssr <iglesiasmaia6@gmail.com>
2022-09-05 15:58:09 +0000

    state update

--- models/state.py
+++ models/state.py
@@ -6,21 +6,24 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 import models
+from os import getenv
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
+    
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
                           cascade="all, delete, delete-orphan")
-
-    @property
-    def cities(self):
-        """getter attribute cities that returns the
-        list of City instances with state_id"""
-        list_cities = []
-        for city in models.storage.all("City").value():
-            if city.state_id == self.id:
-                list_cities.append(city)
-        return list_cities
+    else:
+        @property
+        def cities(self):
+            """getter attribute cities that returns the
+            list of City instances with state_id"""
+            list_cities = []
+            for city in models.storage.all("City").value():
+                if city.state_id == self.id:
+                    list_cities.append(city)
+            return list_cities
